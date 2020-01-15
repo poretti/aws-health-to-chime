951d5836ec249dbbe77829df7c8ac66e8a6a3d77
Michael Poretti <michael.poretti@gmail.com>
2020-01-15 00:52:41 -0800

    Setup health CW event and log via lambda

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,17 +1,35 @@
 import * as cdk from '@aws-cdk/core';
-import lambda = require("@aws-cdk/aws-lambda");
+import * as events from '@aws-cdk/aws-events';
+import * as eventsTargets from '@aws-cdk/aws-events-targets';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class HealthToChimeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, "HealthToChimeLambda", {
+    const lambdaFunction = new lambda.Function(this, "HealthToChimeLambda", {
       runtime: lambda.Runtime.NODEJS_12_X,
+      timeout: cdk.Duration.seconds(15),
       code: lambda.Code.asset("dist/src"),
       handler: "app.handler",
+      deadLetterQueueEnabled: true,
       environment: {
-        myVar: "myVar"
+        chimeRoomWebhook: "some-webhook"
       }
+    });
+
+    new events.Rule(this, "AwsHealthEvents", {
+      description: "Send AWS Health events to the specified target",
+      enabled: true,
+      eventPattern: {
+        // https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/EventTypes.html#health-event-types
+        detailType: ["AWS Health Event"],
+        source: ["aws.health"],
+        detail: {
+          eventTypeCategory: ["issue", "accountNotification", "scheduledChange"]
+        },
+      },
+      targets: [new eventsTargets.LambdaFunction(lambdaFunction)],
     });
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,5 @@
-import { SNSEvent } from 'aws-lambda';
-
-exports.handler = async (event: SNSEvent) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+// TODO: use actual type once it's defined
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/56c1ea26b59ed0e4634b1ba27096ab3b90371875/types/aws-lambda/index.d.ts#L1156
+exports.handler = async (event: any) => {
+    console.log(event);
 };
